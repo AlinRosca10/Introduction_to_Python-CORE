b3f3550f7d7a912005a60c999bf0c4814b32302e
Alin Rosca <alin.rosca545@yahoo.com>
2026-01-05 19:16:23 +0200

    Utilizarea parantezelor si a caracterului \ pentru continuare

--- curs6/afisare_grafica2.py
+++ curs6/afisare_grafica2.py
@@ -16,3 +16,16 @@ a = 1 + 2 + 3 + 4 + 5 + 6 \
     + 7 + 8 + 9 + 113
 print(f"Result is: {a}")
 
+d = (1 + 2 + 3 + 4 + 5 + 6 +
+     7 + 8 + 9 + 10 + 11 + 12)
+print(f"Result is: {d}")
+
+# caracter de continuare '\'
+sum = 50 + 100 + 150 + 200 + 250 + 300 + 500 + 550 + 600 + 650 + \
+      700 + 750 + 800 + 850 + 900 + 950 + 1000
+print(f"Sum is: {sum}")
+
+# paranteze pentru continuare
+total = (25 + 50 + 75 + 100 + 125 + 150 + 175 + 200 +
+         225 + 250 + 275 + 300 + 325 + 350 + 375 + 400)
+print(f"Total is: {total}")
