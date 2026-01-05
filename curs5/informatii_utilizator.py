3b21c1e3972444cb29696bf60ab688cb7b52cf6f
Alin Rosca <alin.rosca545@yahoo.com>
2026-01-05 18:23:42 +0200

    Informatii utilizator in forme diferite spre analiza

---
+++ curs5/informatii_utilizator.py
@@ -0,0 +1,17 @@
+#program care afiseaza informatii despre utilizator
+#inclusiv numele, prenumele, varsta si adresa de email
+
+#Introduceti numele utilizatorului
+nume=input("Introduceti numele dumneavoastra: ")
+#Introduceti prenumele utilizatorului
+prenume=input("Introduceti prenumele dumneavoastra: ")
+#Introduceti varsta utilizatorului
+varsta=input("Introduceti varsta dumneavoastra: ")  
+#Introduceti adresa de email a utilizatorului
+email=input("Introduceti adresa de email: ")
+#Introduceti orasul in care locuieste utilizatorul
+oras=input("Introduceti orasul in care locuiti: ")
+#Introduceti tara in care locuieste utilizatorul
+tara=input("Introduceti tara in care locuiti: ")
+#Listarea unui mesaj de bun venit
+print("Bine ai venit si te salut "+nume+" "+prenume+" din orasul "+oras+"("+tara+")!\n"+"Varsta ta este "+varsta+" ani.\n"+"Adresa ta de email este: "+email+"\n"+"Bun venit in lumea PYTHON!!!Iti dorim o zi cat mai frumoasa!")
---
+++ curs5/informatii_utilizator3.py
@@ -0,0 +1,33 @@
+# Program care afiseaza informatii despre utilizator
+
+# Introduceti numele utilizatorului
+nume = input ("Introduceti numele utilizatorului: ")
+
+# Introduceti prenumele utilizatorului
+prenume = input ("Introduceti prenumele utilizatorului: ")
+
+# Introduceti parola utilizatorului
+parola = input ("Introduceti parola utilizatorului: ")
+
+# Introduceti varsta utilizatorului
+varsta = input ("Introduceti varsta utilizatorului: ")
+
+# Introduceti orasul utilizatorului
+oras = input ("Introduceti orasul utilizatorului: ")
+
+# Introduceti tara utilizatorului
+tara = input ("Introduceti tara utilizatorului: ")
+
+# Introduceti emailul utilizatorului
+email = input ("Introduceti emailul utilizatorului: ")
+
+# Afisati informatiile despre utilizator
+print ("\nInformatii despre utilizator:")
+print ("Nume:", nume)
+print ("Prenume:", prenume)
+print ("Parola:", parola)
+print ("Varsta:", varsta)
+print ("Oras:", oras)
+print ("Tara:", tara)
+print ("Email:", email)
+print ("\nMultumim pentru furnizarea informatiilor!\n")
