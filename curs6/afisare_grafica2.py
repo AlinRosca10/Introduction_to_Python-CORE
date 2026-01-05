0319310b68fa32ef8c03e8bb7853aa1d07697b9f
Alin Rosca <alin.rosca545@yahoo.com>
2026-01-05 19:06:34 +0200

    📘 În Python, regulile de denumire sunt standardizate în principal prin PEP 8 și au rolul de a face codul mai clar, lizibil și ușor de întreținut. Iată principalele aspecte cheie:
    1. Denumirea scripturilor (fișierelor .py)
    Se folosesc litere mici
    Cuvintele se separă cu underscore (_)
    Numele trebuie să fie descriptiv
    
    2. Denumirea variabilelor
    snake_case (litere mici + underscore)
    Nume clare, care indică scopul variabilei
    Evită abrevierile ambigue
    
    3. Denumirea funcțiilor
    Tot snake_case
    Numele trebuie să descrie acțiunea realizată
    
    4. Denumirea claselor
    PascalCase (fiecare cuvânt începe cu literă mare)
    Numele sunt, de regulă, substantive
    
    5. Constante
    Litere mari
    Cuvinte separate prin underscore
    
    6. Convenții speciale
    _variable → utilizare internă
    __variable → name mangling (în clase)
    __init__, __str__ → metode speciale (dunder methods)
    
    Beneficii principale
    
    ✔ Cod mai ușor de citit
    ✔ Colaborare eficientă în echipă
    ✔ Întreținere și depanare mai rapide
    ✔ Respectarea standardelor Python

---
+++ curs6/afisare_grafica2.py
@@ -0,0 +1,18 @@
+x = 5
+y = 3
+z = x + y
+
+print(f"Sum of numbers is: {z}")
+
+a = 10
+b = 2
+res = a * b
+print("Result is: ", res)
+
+a = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+print(f"Result is: {a}")
+
+a = 1 + 2 + 3 + 4 + 5 + 6 \
+    + 7 + 8 + 9 + 113
+print(f"Result is: {a}")
+
